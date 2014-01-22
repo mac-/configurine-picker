84cc01d319b3ba7237edd936c2333c937dec0f15
Mac Angell <mac.angell@pearson.com>
2014-01-22 10:37:11 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,100 @@
 configurine-picker
 ==================
 
-Logic for picking config values based on a given app name, app version, and environment name
+Logic for picking config values based on a given app name, app version, and environment name from Configurine
+
+[![Build Status](https://secure.travis-ci.org/mac-/configurine-picker.png)](http://travis-ci.org/mac-/configurine-picker)
+[![Coverage Status](https://coveralls.io/repos/mac-/configurine-picker/badge.png)](https://coveralls.io/r/mac-/configurine-picker)
+[![NPM version](https://badge.fury.io/js/configurine-picker.png)](http://badge.fury.io/js/configurine-picker)
+[![Dependency Status](https://david-dm.org/mac-/configurine-picker.png)](https://david-dm.org/mac-/configurine-picker)
+
+[![NPM](https://nodei.co/npm/configurine-picker.png?downloads=true&stars=true)](https://nodei.co/npm/configurine-picker/)
+
+## Installation
+
+You may depend on the module by putting an entry in the dependencies section of your package.json file with the appropriate version:
+
+	"configurine-picker": "0.x.x"
+
+You may also use npm to install the module and have it saved to your package.json using:
+
+	npm install configurine-picker --save
+
+## Usage
+
+This module takes a set of config entries and filters them down to one of each name by picking the one whose associations best match the given application and environment using the following priority:
+
+* has both the matching app and env association
+* has one of a matching app or env association (app or env may have a higher priority based on app settings (associationPriority))
+* has no matching app or env associations
+
+The function exported by this module takes one `options` paramter and returns an object that contains the names provided and the corresponding values that were picked. The expected properties on the `options` object are:
+
+* `appName` - the name of the application to match on
+* `appVersion` - the version of the application to match on
+* `environmentName` - the name of the environment to match on
+* `names` - an array of strings that represent the names of the config entries pick for
+* `associationPriority` - tells the picker which association to give priority to: 'app' or 'env'. Defaults to 'app'
+* `entries` - the collection of config entries from configurine to pick from
+
+Example:
+
+```javascript
+var picker = require('configurine-picker');
+
+// example set of config entries from configurine for demo purposes
+var entries = [
+	{
+		name: 'foo',
+		value: 'bar',
+		associations: {
+			applications: [{
+				name: 'my-app',
+				versions: ['1.0.0']
+			}],
+			environments: []
+		},
+		isActive: true,
+		isSensitive: false,
+		owner: 'me'
+	},
+	{
+		name: 'foo',
+		value: 'baz',
+		associations: {
+			applications: [],
+			environments: ['production']
+		},
+		isActive: true,
+		isSensitive: false,
+		owner: 'me'
+	}
+];
+
+var config = picker({
+	names: ['foo'],
+	appName: 'my-app',
+	appVersion: '1.0.0',
+	environmentName: 'production',
+	associationPriority: 'app',
+	entries: entries
+});
+
+console.log(config.foo); // 'bar'
+
+
+config = picker({
+	names: ['foo', 'other'],
+	appName: 'my-app',
+	appVersion: '1.0.0',
+	environmentName: 'production',
+	associationPriority: 'env',
+	entries: entries
+});
+
+console.log(config.foo); // 'baz'
+console.log(config.other); // undefined
+```
+
+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/mac-/configurine-picker/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
+
---
+++ test/unit/picker.tests.js
@@ -0,0 +1,347 @@
+var assert = require('assert'),
+	_ = require('underscore'),
+	picker = require('../../lib/index.js');
+	
+
+describe('Picker Unit Tests', function() {
+
+	it('should pick entry that has matching env and app association regardless of priority', function(done) {
+		var appName = 'app1',
+			appVersion = '1.0.0',
+			envName = 'dev',
+			names = ['name1', 'name2'],
+			mockConfigurineResults = [
+				{
+					name: 'name1',
+					value: 'fnord1',
+					associations: {
+						applications: [
+							{
+								name: 'app1',
+								versions: ['1.0.0']
+							}
+						],
+						environments: ['dev']
+					}
+				},
+				{
+					name: 'name1',
+					value: 'fnord2',
+					associations: {
+						applications: [],
+						environments: ['dev']
+					}
+				},
+				{
+					name: 'name1',
+					value: 'fnord3',
+					associations: {
+						applications: [
+							{
+								name: 'app1',
+								versions: ['1.0.0', '2.0.0']
+							}
+						],
+						environments: ['stg']
+					}
+				},
+				{
+					name: 'name1',
+					value: 'fnord4',
+					associations: {
+						applications: [
+							{
+								name: 'app1',
+								versions: ['1.0.0']
+							}
+						],
+						environments: []
+					}
+				},
+				{
+					name: 'name1',
+					value: 'fnord5',
+					associations: {
+						applications: [],
+						environments: []
+					}
+				}
+			];
+
+		var config = picker({
+			names: names,
+			appName: appName,
+			appVersion: appVersion,
+			environmentName: envName,
+			associationPriority: 'app',
+			entries: mockConfigurineResults
+		});
+
+		assert.strictEqual(config.name1, 'fnord1');
+		assert.strictEqual(config.name2, undefined);
+
+		config = picker({
+			names: names,
+			appName: appName,
+			appVersion: appVersion,
+			environmentName: envName,
+			associationPriority: 'env',
+			entries: mockConfigurineResults
+		});
+		
+		assert.strictEqual(config.name1, 'fnord1');
+		assert.strictEqual(config.name2, undefined);
+
+		done();
+	});
+
+	it('should pick entry that has matching app over matching env when app is priority', function(done) {
+		var appName = 'app1',
+			appVersion = '1.0.0',
+			envName = 'dev',
+			names = ['name1', 'name2'],
+			mockConfigurineResults = [
+				{
+					name: 'name1',
+					value: 'fnord2',
+					associations: {
+						applications: [],
+						environments: ['dev']
+					}
+				},
+				{
+					name: 'name1',
+					value: 'fnord3',
+					associations: {
+						applications: [
+							{
+								name: 'app1',
+								versions: ['1.0.0', '2.0.0']
+							}
+						],
+						environments: ['stg']
+					}
+				},
+				{
+					name: 'name1',
+					value: 'fnord4',
+					associations: {
+						applications: [
+							{
+								name: 'app1',
+								versions: ['1.0.0']
+							}
+						],
+						environments: []
+					}
+				},
+				{
+					name: 'name1',
+					value: 'fnord5',
+					associations: {
+						applications: [],
+						environments: []
+					}
+				}
+			];
+
+		var config = picker({
+			names: names,
+			appName: appName,
+			appVersion: appVersion,
+			environmentName: envName,
+			associationPriority: 'app',
+			entries: mockConfigurineResults
+		});
+		
+		assert.strictEqual(config.name1, 'fnord4');
+		assert.strictEqual(config.name2, undefined);
+		done();
+	});
+
+	it('should pick entry that has matching env over matching app when env is priority', function(done) {
+		var appName = 'app1',
+			appVersion = '1.0.0',
+			envName = 'dev',
+			names = ['name1', 'name2'],
+			mockConfigurineResults = [
+				{
+					name: 'name1',
+					value: 'fnord2',
+					associations: {
+						applications: [],
+						environments: ['dev']
+					}
+				},
+				{
+					name: 'name1',
+					value: 'fnord3',
+					associations: {
+						applications: [
+							{
+								name: 'app1',
+								versions: ['1.0.0', '2.0.0']
+							}
+						],
+						environments: ['stg']
+					}
+				},
+				{
+					name: 'name1',
+					value: 'fnord4',
+					associations: {
+						applications: [
+							{
+								name: 'app1',
+								versions: ['1.0.0']
+							}
+						],
+						environments: []
+					}
+				},
+				{
+					name: 'name1',
+					value: 'fnord5',
+					associations: {
+						applications: [],
+						environments: []
+					}
+				}
+			];
+
+		var config = picker({
+			names: names,
+			appName: appName,
+			appVersion: appVersion,
+			environmentName: envName,
+			associationPriority: 'env',
+			entries: mockConfigurineResults
+		});
+		
+		assert.strictEqual(config.name1, 'fnord2');
+		assert.strictEqual(config.name2, undefined);
+		done();
+	});
+
+	it('should pick the only entry when there is only one', function(done) {
+		var appName = 'app1',
+			appVersion = '1.0.0',
+			envName = 'dev',
+			names = ['name1', 'name2'],
+			mockConfigurineResults = [
+				{
+					name: 'name1',
+					value: 'fnord2',
+					associations: {
+						applications: [],
+						environments: ['dev']
+					}
+				},
+				{
+					name: 'name2',
+					value: 'fnord3',
+					associations: {
+						applications: [
+							{
+								name: 'app1',
+								versions: ['1.0.0', '2.0.0']
+							}
+						],
+						environments: []
+					}
+				}
+			];
+
+		var config = picker({
+			names: names,
+			appName: appName,
+			appVersion: appVersion,
+			environmentName: envName,
+			associationPriority: 'app',
+			entries: mockConfigurineResults
+		});
+		
+		assert.strictEqual(config.name1, 'fnord2');
+		assert.strictEqual(config.name2, 'fnord3');
+		done();
+	});
+
+	it('should return an empty object when no names are provided', function(done) {
+		var appName = 'app1',
+			appVersion = '1.0.0',
+			envName = 'dev',
+			names = [],
+			mockConfigurineResults = [
+				{
+					name: 'name1',
+					value: 'fnord2',
+					associations: {
+						applications: [],
+						environments: ['dev']
+					}
+				},
+				{
+					name: 'name2',
+					value: 'fnord3',
+					associations: {
+						applications: [
+							{
+								name: 'app1',
+								versions: ['1.0.0', '2.0.0']
+							}
+						],
+						environments: []
+					}
+				}
+			];
+
+		var config = picker({
+			names: names,
+			appName: appName,
+			appVersion: appVersion,
+			environmentName: envName,
+			associationPriority: 'app',
+			entries: mockConfigurineResults
+		});
+		
+		assert.strictEqual(_.keys(config).length, 0);
+		assert.strictEqual(config.name1, undefined);
+		done();
+	});
+
+	it('should throw an error when provided missing or invalid params', function(done) {
+		var names = ['name1', 'name2'],
+			mockConfigurineResults = [
+				{
+					name: 'name1',
+					value: 'fnord2',
+					associations: {
+						applications: [],
+						environments: ['dev']
+					}
+				},
+				{
+					name: 'name2',
+					value: 'fnord3',
+					associations: {
+						applications: [
+							{
+								name: 'app1',
+								versions: ['1.0.0', '2.0.0']
+							}
+						],
+						environments: []
+					}
+				}
+			];
+
+		assert.throws(function() {
+			picker({
+				names: names,
+				associationPriority: 'app',
+				entries: mockConfigurineResults
+			});
+		}, /missing/);
+		done();
+	});
+});
